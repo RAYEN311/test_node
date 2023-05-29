e115f9e02606b048b4000db00fb2c950b3d50a1f
RAYEN311 <129397918+RAYEN311@users.noreply.github.com>
2023-05-29 21:35:55 +0100

    Update server4567812324598745.js

--- server4567812324598745.js
+++ server4567812324598745.js
@@ -1,6 +1,6 @@
 // requiring the framework express
 const express = require('express')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4040
 const app = express()
 
 // biew engine used 
